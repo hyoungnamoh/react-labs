aebc1428c0fe1a721ef1bf1f9c10a3b9f14a2326
hyungnam <zzzsh789@naver.com>
2020-07-16 23:33:03 +0900

    테스트 커밋

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -6,7 +6,7 @@ export default function firstPost() {
             <h1>First Post</h1>
             <h2>
                 <Link href="/">
-                    <a>Back to home</a>
+                    <a>Back to home!</a>
                 </Link>
             </h2>
         </>
