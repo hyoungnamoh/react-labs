637cc88167b8936636dec315f8cc3d72aa7b398b
hyungnam <zzzsh789@naver.com>
2020-07-18 23:59:20 +0900

    css 추가

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,14 +1,19 @@
 import Link from 'next/link';
+import Header from 'next/head';
+import Layout from '../../components/layout';
+
 
 export default function firstPost() {
-    return (
-        <>
-            <h1>First Post</h1>
-            <h2>
-                <Link href="/">
-                    <a>Back to home!</a>
-                </Link>
-            </h2>
-        </>
-    );
+  return (
+    <Layout>
+      <Header>
+        <title>First Post</title>
+      </Header>
+      <h2>
+        <Link href="/">
+          <a>Back to home!</a>
+        </Link>
+      </h2>
+    </Layout>
+  );
 }
