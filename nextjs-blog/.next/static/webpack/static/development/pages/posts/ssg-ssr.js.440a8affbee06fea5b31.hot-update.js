bf89db352ba6d068b9de4eaaca42b5bc2a2c91eb
오형남 <zzzsh789@naver.com>
2020-07-19 16:45:11 +0900

    Add: getStaticProps, Dynamic Routes

---
+++ nextjs-blog/.next/static/webpack/static/development/pages/posts/ssg-ssr.js.440a8affbee06fea5b31.hot-update.js
@@ -0,0 +1,36 @@
+webpackHotUpdate("static/development/pages/posts/ssg-ssr.js",{
+
+/***/ "./pages/posts/ssg-ssr.js":
+/*!********************************!*\
+  !*** ./pages/posts/ssg-ssr.js ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _this = undefined,
+    _jsxFileName = "/Users/ohhyoungnam/Desktop/study/react-labs/nextjs-blog/pages/posts/ssg-ssr.js";
+
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+var ssgSsr = function ssgSsr() {
+  return __jsx("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3,
+      columnNumber: 5
+    }
+  }, "\uD558\uC774");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (ssgSsr);
+
+/***/ })
+
+})
+//# sourceMappingURL=ssg-ssr.js.440a8affbee06fea5b31.hot-update.js.map
