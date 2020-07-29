dde92ed561d76fc23871391a45efb59992a1996f
hyungnam <zzzsh789@naver.com>
2020-07-29 23:56:02 +0900

    update

--- nextjs/ssr-native/server/Html.js
+++ nextjs/ssr-native/server/Html.js
@@ -1,17 +1,18 @@
 
 import React from 'react';
 
-const Html = (props) => (
-  <html lang="en">
-    <head>
+const Html = ({preloadState, script, appComponent}) => (
+  <html className="no-js" lang="en">
+  <head>
       <meta charSet="utf-8" />
+      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <title>React Server-Side Rendering</title>
-      <script dangerouslySetInnerHTML={{ __html: props.preloadState }}></script>
-    </head>
-    <body>
-      <div id="root">{props.children}</div>
-      <script src={props.script}></script>
-    </body>
+      <script dangerouslySetInnerHTML={{ __html: preloadState }}></script>
+  </head>
+  <body>
+  <div id="root" dangerouslySetInnerHTML={{ __html: appComponent }} />
+  <script src={script} />
+  </body>
   </html>
 );
 
--- nextjs/ssr-native/server/index.js
+++ nextjs/ssr-native/server/index.js
@@ -1,31 +1,35 @@
-import express from 'express';
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import App from '../client/App';
-import Html from './Html';
+// Express
+import express from 'express'
+
+// React
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import App from '../client/App'
+import Html from './Html'
+import WDM from './WDM'
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res, next) => {
-  let preloadState = {
-    text: 'Sever-Side-Rendering',
-  };
+app.use(WDM);
 
-  let renderProps = {
-    //preloadState를 window객체를 통해 전달
-    preloadState: `window.__PRELOADED_STATE__=${JSON.stringify(preloadState).replace(/</g, '\\u003c')}`, 
-    script: '/build/client.bundle.js'
-  }
+app.get('/',function(req, res, next){
 
-  ReactDOMServer.renderToNodeStream(
-    <Html {...renderProps}>
-      <App data={preloadState}/>
-    </Html>
-  ).pipe(res);
-res.send('<h1>Hello~!! React Server-Side Rendering</h1>');
+    let preloadState = {
+        text : 'Server-Side Rendering'
+    };
+
+    let renderProps = {
+        preloadState: `window.__PRELOADED_STATE__ =${JSON.stringify(preloadState).replace(/</g, '\\u003c')}`,
+        script: 'http://localhost:3000/build/client.bundle.js',
+        appComponent: ReactDOMServer.renderToString(<App data={preloadState}/>)
+    };
+
+    const html = ReactDOMServer.renderToStaticMarkup(<Html {...renderProps}/>); // server-side Rendering
+
+    res.send(`<!doctype html>${html}`);
 });
 
-app.listen(port, () => {
-console.log('http://localhost:3000')
+app.listen(port, ()=>{
+    console.log('http://localhost:3000')
 });
